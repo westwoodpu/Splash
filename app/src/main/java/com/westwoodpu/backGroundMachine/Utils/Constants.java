21ee2e80d026cfa6fd2e70ddc96895256d7077d9
westwoodpu <westwoodpu@gmail.com>
2018-05-15 17:17:53 -0700

    #update the API endpoint, the old one is not valid anymore #update the version to 1.01 to release on Playstore

--- app/src/main/java/com/westwoodpu/backGroundMachine/Utils/Constants.java
+++ app/src/main/java/com/westwoodpu/backGroundMachine/Utils/Constants.java
@@ -6,5 +6,6 @@ package com.westwoodpu.backGroundMachine.Utils;
 
 public class Constants {
     public final static String BASE_API_URL = "https://api.unsplash.com/";
-    public final static String APPLICATION_ID = "b76ed52ac15a9d61f21bfff5fbef58ddd0d522b0de466274fc1c0cfb6a2e4cba";
+//    public final static String APPLICATION_ID = "b76ed52ac15a9d61f21bfff5fbef58ddd0d522b0de466274fc1c0cfb6a2e4cba";
+    public final static String APPLICATION_ID = "70db4e738654d5af4e0701a37cf980e50f4a0f39fc6811feedc81ebc83b66883";
 }
